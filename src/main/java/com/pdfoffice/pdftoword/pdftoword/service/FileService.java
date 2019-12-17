3389213d11625324ffe1b31d2f3b8eb75a04c8a9
shubham <shubhamr837@gmail.com>
2019-12-17 16:34:01 +0530

    added function to convert pdf to word.

--- src/main/java/com/pdfoffice/pdftoword/pdftoword/service/FileService.java
+++ src/main/java/com/pdfoffice/pdftoword/pdftoword/service/FileService.java
@@ -2,17 +2,20 @@ package com.pdfoffice.pdftoword.pdftoword.service;
 
 import com.pdfoffice.pdftoword.pdftoword.config.ServiceConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 
+@Component
 public class FileService {
     private ServiceConfig serviceConfig;
     @Autowired
     public FileService(ServiceConfig serviceConfig){
         this.serviceConfig = serviceConfig;
     }
-    public File createFile(){
-        return new File(System.getProperty("user.dir"));
+
+    public File createFile(String type) {
+        return new File(System.getProperty("user.dir"), "document." + type);
     }
     public void getFile(){
 
--- src/main/java/com/pdfoffice/pdftoword/pdftoword/service/PdfToWordService.java
+++ src/main/java/com/pdfoffice/pdftoword/pdftoword/service/PdfToWordService.java
@@ -1,5 +1,66 @@
 package com.pdfoffice.pdftoword.pdftoword.service;
 
-public class PdfToWordService {
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.xwpf.usermodel.BreakType;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+@RestController
+public class PdfToWordService {
+    @Autowired
+    private FileService fileService;
+
+    @GetMapping("/doc")
+    public String pdftoword(@RequestParam("file") MultipartFile file) throws IOException {
+        File pdf_file = fileService.createFile("pdf");
+
+        try (InputStream is = file.getInputStream()) {
+            Files.copy(is, pdf_file.toPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        PDDocument pdfDocument = null;
+        pdfDocument = PDDocument.load(pdf_file);
+
+        if (pdfDocument == null) {
+            return "error";
+        }
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        XWPFDocument document = new XWPFDocument();
+        int no_of_pages = pdfDocument.getNumberOfPages();
+
+        for (int i = 1; i <= no_of_pages; i++) {
+            pdfStripper.setStartPage(i);
+            pdfStripper.setEndPage(i);
+            pdfStripper.setSortByPosition(true);
+            String text = pdfStripper.getText(pdfDocument);
+
+            XWPFParagraph paragraph = document.createParagraph();
+            XWPFRun para_run = paragraph.createRun();
+            para_run.addBreak(BreakType.PAGE);
+            para_run.setText(text);
+        }
+        File doc_file = fileService.createFile("doc");
+        FileOutputStream out = new FileOutputStream(doc_file);
+        document.write(out);
+        document.close();
+        pdfDocument.close();
+        pdf_file.delete();
+
+        return doc_file.getAbsolutePath();
+    }
 }
