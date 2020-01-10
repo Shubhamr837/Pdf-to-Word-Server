22ed63071365c1972ec8341f059852488f8d7f89
shubhamr837 <sremail2010@gmail.com>
2020-01-10 19:55:59 +0530

    add configuration for aws ecs

--- src/main/java/com/pdfoffice/pdftoword/pdftoword/PdfToWordApplication.java
+++ src/main/java/com/pdfoffice/pdftoword/pdftoword/PdfToWordApplication.java
@@ -3,13 +3,22 @@ package com.pdfoffice.pdftoword.pdftoword;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class PdfToWordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdfToWordApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public ResponseEntity healthcheck() {
+		return ResponseEntity.ok().build();
 	}
 
 }
