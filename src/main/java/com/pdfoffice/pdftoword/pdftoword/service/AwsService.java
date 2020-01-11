cd4fcf538999ed58cc6a1817dd6e4ae4976df8c0
shubhamr837 <sremail2010@gmail.com>
2020-01-11 18:56:34 +0530

    add configuration for aws ecs

--- src/main/java/com/pdfoffice/pdftoword/pdftoword/service/AwsService.java
+++ src/main/java/com/pdfoffice/pdftoword/pdftoword/service/AwsService.java
@@ -24,7 +24,7 @@ public class AwsService {
     private AmazonS3 s3Client;
 
     public AwsService() {
-        credentials = new BasicAWSCredentials(System.getenv("AWS_ACCESS_ID"), System.getenv("AWS_SECRET_KEY"));
+        credentials = new BasicAWSCredentials(System.getenv("AWS_SECRET_ACCESS_KEY"), System.getenv("AWS_ACCESS_KEY_ID"));
         s3Client = AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.AP_SOUTH_1)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
