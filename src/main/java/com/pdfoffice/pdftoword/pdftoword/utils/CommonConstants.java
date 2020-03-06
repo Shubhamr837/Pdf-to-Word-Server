c40a893329e0ff5132a33e2a5c6f2285e86552c9
Subham raj <45128519+Shubhamr837@users.noreply.github.com>
2020-03-06 13:04:29 +0530

    Update CommonConstants.java

--- src/main/java/com/pdfoffice/pdftoword/pdftoword/utils/CommonConstants.java
+++ src/main/java/com/pdfoffice/pdftoword/pdftoword/utils/CommonConstants.java
@@ -1,7 +1,7 @@
 package com.pdfoffice.pdftoword.pdftoword.utils;
 
 public class CommonConstants {
-    public static final String BUCKET_NAME = "shubhamr837-pdf-reader";
+    public static final String BUCKET_NAME = "shubhamr837pdf-reader";
     public static final String AWS_ACCESS_ID = System.getenv("AWS_ACCESS_ID");
     public static final String AWS_SECRET_KEY = System.getenv("AWS_SECRET_KEY");
 }
